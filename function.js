9f2be2d336ad4d8f50797c08a783450d3ded002f
ajaykn_04 <ajaykn04@gmail.com>
2024-07-01 16:31:29 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,12 @@
+function sum(a,b){
+    console.log(a+b);
+}
+sum(5,10);
+
+function fullname(fname,lname){
+    console.log(`${fname} ${lname}`);
+    
+}
+fname="ajay";
+lname="das";
+fullname(fname,lname);
---
+++ while.js
@@ -0,0 +1,8 @@
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+let sum=0;
+let i=0;
+while(i<numbers.length){
+    sum=sum+numbers[i];
+    i++;
+}
+console.log(sum);
