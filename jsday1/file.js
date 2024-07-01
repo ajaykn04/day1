d9a445544cd6feeb10b5c1b4f381ea45b091b08b
Ajay Das <134246618+ajaykn04@users.noreply.github.com>
2024-07-01 16:33:27 +0530

    Add files via upload

---
+++ jsday1/file.js
@@ -0,0 +1,38 @@
+// let person = {
+//     name: "Tiya",
+//     age: 10,
+//     address: {
+//         housename: "kinar",
+//         postoffice: "kulam"
+//     }
+// };
+
+// console.log(person.name);
+// console.log(person.address.housename);
+// console.log(person.name,person.age);
+
+// --------------------------------------
+
+// let fruit=["manga","thenga","chakka","muringa","venda"];
+// fruit.unshift("mathan");
+
+// -------------------------
+
+// let people = [
+//     {
+//         name: "ajay",
+//         class: "s4"
+//     },
+//     {
+//         name: "adithya",
+//         class: "s4"
+//     },
+//     {
+//         name: "aswin",
+//         class: "s4"
+//     }
+// ]
+// console.log(people[0]);
+
+// ---------------------------
+
